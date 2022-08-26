a2dcb1efa33fa670fedc38d0c2aa4282d86794cb
CHHG9999 <choione999@naver.com>
2022-08-26 11:08:14 +0900

    08/26 병합 전 커밋

--- src/main/java/kr/human/camping/controller/ReservationController.java
+++ src/main/java/kr/human/camping/controller/ReservationController.java
@@ -1,6 +1,10 @@
 package kr.human.camping.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -12,6 +16,7 @@ import kr.human.camping.service.ReservationService;
 import kr.human.camping.service.RoomService;
 import kr.human.camping.vo.Comm2VO;
 import kr.human.camping.vo.CompanyVO;
+import kr.human.camping.vo.MemberVO;
 import kr.human.camping.vo.ReservationVO;
 import kr.human.camping.vo.RoomVO;
 import lombok.extern.slf4j.Slf4j;
@@ -20,6 +25,7 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 public class ReservationController {
 
+	
 	@Autowired
 	private ReservationService reservationService;
 	
@@ -28,22 +34,26 @@ public class ReservationController {
 	
 	// 예약페이지
 	@RequestMapping(value ="/reservation", method = {RequestMethod.GET})
-		public String selectList(
-				@RequestParam("roomidx") int roomidx, // 내가 선택한 방의 roomidx값
-//				HttpServletRequest request, // 세션에서 값을 가져오는 방법	
-//				HttpSession session,  // 세션에서 값을 가져오는 방법	
-				Model model){
-		int idx = roomService.companyidx(roomidx);
-		RoomVO rvo = roomService.selectRoom(roomidx);
-		CompanyVO cvo = reservationService.selectCompany(idx);
-				
-//		 String id = request.getParameter("id");
-//		 String sessionId = (String) session.getAttribute("id");// 세션에서 값을 가져오는 방법	
-
-		model.addAttribute("rvo",rvo);
-		model.addAttribute("roomidx",roomidx);
-		model.addAttribute("cvo",cvo);
-		return "reservation/reservation";
+		public String selectList(@RequestParam("roomidx") int roomidx, // 내가 선택한 방의 roomidx값
+				Model model,
+				HttpServletRequest request){
+	
+		HttpSession session = request.getSession(); // 세션영역에 있는 객체를 생성하여
+		// 세션에 있는 MemberInfo를 vo에 담는다.
+		MemberVO userVo = (MemberVO)session.getAttribute("UserInfo");  //userDetail 객체를 가져옴
+		log.info("userVo 실행결과 : " + userVo);
+		if(userVo != null) {
+			int idx = roomService.companyidx(roomidx);
+			RoomVO rvo = roomService.selectRoom(roomidx);
+			CompanyVO cvo = reservationService.selectCompany(idx);
+			
+			model.addAttribute("userid", userVo.getId());      //유저 아이디	
+			model.addAttribute("roomidx",roomidx);
+			model.addAttribute("rvo",rvo);
+			model.addAttribute("cvo",cvo);
+			return "reservation/reservation";
+		}
+		return "/login";
 	}
 	
 	// 저장/삭제  수정기능은 없습니다.
